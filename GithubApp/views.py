ab396b0e6b7f0b3a4710de15c749c0d884258302
ernstharnie <ernst_harnie@hotmail.com>
2016-04-21 11:17:48 +0200

    Added bootstrap to project

--- GithubApp/views.py
+++ GithubApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 
 # Create your views here.
 def index(self):
-	return HttpResponse("<h1>Hello World</h1>");
+	return render(self, "GithubApp/index.html", {})
+	#return HttpResponse('<button type="button" class="btn btn-success">Success</button>');
